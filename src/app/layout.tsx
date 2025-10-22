af03f07b1f11a8fbaeba9a01e87f45c75510ad0f
ITSimaoAbel <simao3cuco@gmail.com>
2025-10-22 14:02:22 +0200

    Refactor code structure for improved readability and maintainability

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,3 +1,4 @@
+import Footer from "./components/Footer";
 import "./globals.css";
 
 export default function RootLayout({
@@ -11,6 +12,7 @@ export default function RootLayout({
         className={`antialiased`}
       >
         {children}
+       <Footer />
       </body>
     </html>
   );
